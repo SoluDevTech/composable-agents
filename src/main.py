6d63953e175465dfaf2c68099d6781440a50f924
Kaiohz <yohan.goncalves@cosigma.io>
2026-04-24 06:51:49 +0200

    feat: add allowed orgins env var

--- src/main.py
+++ src/main.py
@@ -124,7 +124,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8030"],
+    allow_origins=settings.allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
